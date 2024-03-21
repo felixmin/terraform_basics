09fc193bf3413530fa066cb95e7aee46c9de17c1
felixmin <felix.minze@live.de>
2024-03-21 14:03:47 +0100

    updated infra_test and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# terraform_basics
+# Terraform basics
+
+I created this repository when making my first steps with terraform. The plan is to build a collection of templates for simple tasks.
+
+## Simple vpc with public and private subnet containing a private/public instance
+
+In infra_test I created a simple infrastructure, with the goal to have a private instance without public IP that is still able to access the web. I created this template while struggling to connect a lambda function to the web and this helped me along the way.
--- infra_test/main.tf
+++ infra_test/main.tf
@@ -5,26 +5,57 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
     cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "my_vpc"
+        Project = "terraform-basics"
+    }
 }
 
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "public_subnet"
+        Project = "terraform-basics"
+    }
 }
 
 resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "private_subnet"
+        Project = "terraform-basics"
+    }
 }
 
 resource "aws_internet_gateway" "my_igw" {
     vpc_id = aws_vpc.my_vpc.id
+    tags = {
+        Name = "my_igw"
+        Project = "terraform-basics"
+    }
+}
+
+resource "aws_eip" "rag_nat_eip" {
+  vpc = true
+
+
+  tags = {
+    Name = "rag_nat_eip"
+    Project = "terraform-basics"
+  }
 }
 
 resource "aws_nat_gateway" "my_nat" {
-    allocation_id = "eipalloc-09dd384582328d8e7" # TODO make this dynamic
+    allocation_id = aws_eip.rag_nat_eip.id
     subnet_id = aws_subnet.public_subnet.id
     connectivity_type = "public"
+    tags = {
+        Name = "my_nat"
+        Project = "terraform-basics"
+    }
 }
 
 resource "aws_route_table" "public_route_table" {
@@ -33,6 +64,10 @@ resource "aws_route_table" "public_route_table" {
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.my_igw.id
+    }
+    tags = {
+        Name = "public_route_table"
+        Project = "terraform-basics"
     }
 }
 
@@ -48,6 +83,10 @@ resource "aws_route_table" "private_route_table" {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_nat_gateway.my_nat.id
     }
+    tags = {
+        Name = "private_route_table"
+        Project = "terraform-basics"
+    }
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
@@ -56,41 +95,63 @@ resource "aws_route_table_association" "private_route_table_association" {
 }
 
 resource "aws_key_pair" "deployer_key" {
-  key_name   = "deployer-key"
-  public_key = file("/Users/f.minzenmay/data/rag-chatbot/mykey.pub")
+  key_name   = "tf-basics-deployer-key"
+  public_key = file("/Users/f.minzenmay/data/rag-chatbot/my-key2.pub")
+  tags = {
+    Name = "tf-basics-deployer_key"
+    Project = "terraform-basics"
+  }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_everything" {
+  name        = "allow_everything"
+  description = "Allow traffic"
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_ssh"
+    Name = "allow_everything"
+    Project = "terraform-basics"
   }
 }
 
 
-resource "aws_instance" "public_instance" {
+resource "aws_instance" "terraform_basics_private_instance" {
+    subnet_id = aws_subnet.private_subnet.id
+    ami = "ami-0793a9d76284434e5"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.deployer_key.key_name
+    associate_public_ip_address = false
+    vpc_security_group_ids = [aws_security_group.allow_everything.id]
+    tags = {
+        Name = "private_instance"
+        Project = "terraform-basics"
+    }
+}
+
+resource "aws_instance" "terraform_basics_public_instance" {
     subnet_id = aws_subnet.public_subnet.id
     ami = "ami-0793a9d76284434e5"
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer_key.key_name
     associate_public_ip_address = true
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    vpc_security_group_ids = [aws_security_group.allow_everything.id]
+    tags = {
+        Name = "public_instance"
+        Project = "terraform-basics"
+    }
 }
 
